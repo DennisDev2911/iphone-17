80520c4de6a238564b801322a264fc527d3c3d45
DennisDev2911 <dennis291185wx@gmail.com>
2026-01-05 19:58:44 -0300

    enviando projeto para deploy

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/iphone/react_vite_deploy',
+  //base: "/react_vite_deploy",
 });
